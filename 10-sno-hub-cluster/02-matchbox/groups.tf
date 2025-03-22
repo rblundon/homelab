e7f35053700b2bca6a0277532b3c8fc29cb9bfd1
rblundon <ryan.blundon@protonmail.com>
2025-03-22 13:34:41 -0500

    Clean up directory structure and automate dns with ansible

--- 10-sno-hub-cluster/02-matchbox/groups.tf
+++
@@ -1,8 +0,0 @@
-// Matcher group for OCP Internal machinesmachines
-resource "matchbox_group" "ocp-hub" {
-  name = "ocp-hub" # Physical Server
-  profile = matchbox_profile.hub.name
-  selector = {
-    mac = "98:b7:85:1e:c6:f1" # PXE boots and installs to 10GbE NIC
-  }
-}
---
+++ 10-sno-hub-cluster/03-matchbox/terraform.tfvars
@@ -0,0 +1,2 @@
+matchbox_http_endpoint = "http://matchbox.int.mk-labs.cloud:8080"
+matchbox_rpc_endpoint = "matchbox.int.mk-labs.cloud:8081"
